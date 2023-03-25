f8fd50accabeb56086022308bd0ad24d4c13e493
AqsaHanif <121718004+Aqsaha@users.noreply.github.com>
2023-03-26 02:58:19 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+alert("Asslam O Alakium");
+alert("I Hope you are fine");
+var name = "Aqsa Here";
+alert(name);
+alert("Happy coding")
+var age = "25";
+var newAge = age - 5;
+alert(newAge);
+var age = "25";
+var newAge = age - 5;
+alert(9 % 4)
+var Certificate = "Certificate of Mobile And Development";
+alert("Certified Mobile Appliation And Development");
+var email = "My Email Is 12345@gmail.com"
+alert(email);
+var message = "pizza\n pizz\n piz\n pi \n p";
+alert(message);
+var message = "I am trying to learn javascript\n from the Book of Smart way to learn javascript";
+alert(message);
+var message = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert(message)
+var message = "I am 20 years old";
+alert(message)
+var message = "Never Giveup : )";
+alert(message)
+var num = "You have visited this website 10 times..!..!";
+alert(num);
+var birthYear=prompt("Enter Your birth year :");
+var currentYear=prompt("Enter the current year :");
+
+document.write("Your age is :" + (birthYear-currentYear));
+
+var dollar=279.30;
+var riyal=74.38;
+document.write("Total currency in PKR" ,(dollar*279)+(riyal*74));
+var celsius=25;
+document.write("25C is ",(celsius*9/5)+32 ,"F"+ "<br>");
+var fahrenheit=70;
+document.write("70F is " +(fahrenheit-32)*5/9+"C");
+
+document.write("Price of item1 is 650","<br>"+"Quantity of item1 is 3","<br>");
+document.write("Price of item2 is 100","<br>"+"Quantity of item2 is 7","<br>");
+document.write("Shopping charge is 100","<br>");
+var item1=650*3;
+var item2=100*7;
+var shoppingCharge=100;
+document.write("Total cost of your cart is ",item1+item2+shoppingCharge)
+
